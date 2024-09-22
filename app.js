0268ed2a98fe352eff999d0b779d554b25e519bf
Samuel Habib <samuel.15.fahim@gmail.com>
2024-09-21 21:57:54 -0400

    End bar added, Divider not working

--- app.js
+++ app.js
@@ -22,10 +22,12 @@ function read(filePath){
   if(!filePath.endsWith('.csv')) throw new Error("incorrect file format");
   fs.readFile(filePath, 'utf8', (err, content) => {
       if (err) throw new Error('Error reading the file')
-      const array = Arr(content, filePath) 
+      const array = Arr(content, filePath)
       const max = maxLength(array)
-      const maxCol = maxLengthInCol(array)
-      makeTable(array, max, maxCol)
+      const maxColVar = maxLengthInCol(array)
+      const diffArr = diff(array, maxColVar)
+      var divLength = divderlength(diffArr, max)
+      makeTable(array, diffArr, divLength)
 
   });
 }
@@ -92,54 +94,94 @@ function maxLengthInCol(Arr){
   const max = []
   for(let i =0; i<countArr.length; i++) {
     max.push(Math.max(...countArr[i]))
+    // the Math.max requires the ... operator to work on arrays
   }
 
   return max
 
 }
 
-function makeTable(Arr, maxLength, maxColVar){
-  var spaces = ""
-  var count = 0
-  var divider = "--------"
-
-  for(let i = 0; i <maxLength; i++){
-    divider = divider + "-"
-  }
-
-  // Creates a 2D array with the differences from the max length in each column from the current length
-  let diff = new Array(Arr.length); 
-  for (let i = 0; i < Arr.length; i++) {
-      diff[i] = new Array(Arr[i].length).fill(0); 
-      // MAKE SURE TO INITIALIZE IF YOU ARE ACCESSING
-      // otherwise you must push values into the 2d array
-  }
-  console.log(diff)
-
-  for (let i = 0; i < Arr.length; i++) {
-    for (let j = 0; j < Arr[i].length; j++) {
-      if(Arr[i][j].length == maxColVar[j]){
-        continue
-      } else{
-      diff[i][j] = maxColVar[j]- Arr[i][j].length
+function diff(Arr, maxColVar){
+    // This function does not handle spaces, that is done directly in makeTable
+    // Creates a 2D array with the differences from the max length in each column from the current length
+    let diffArr = new Array(Arr.length); 
+    for (let i = 0; i < Arr.length; i++) {
+      diffArr[i] = new Array(Arr[i].length).fill(0); 
+        // MAKE SURE TO INITIALIZE IF YOU ARE ACCESSING
+        // otherwise you must push values into the 2d array
+    }
+    console.log(diffArr)
+  
+    for (let i = 0; i < Arr.length; i++) {
+      for (let j = 0; j < Arr[i].length; j++) {
+        if(Arr[i][j].length == maxColVar[j]){
+          continue
+        } else{
+          diffArr[i][j] = maxColVar[j]- Arr[i][j].length
+        }
       }
     }
+    return diffArr
+}
+
+function divderlength(diff, maxLength){
+  // bars + maxlength + spaces + 1 space per column
+  var rowLen  = diff[0].length*2
+  console.log(rowLen)
+  var rowTotalSpace = []
+  var currRowSpace = 0
+  var divider = ""
+
+  // account for rowLength
+  for (let i = 0; i < (rowLen); i++) {
+    // divide by 2 because just the length of the row doesn't add that much
+    divider += "-"
   }
+
+  // add the maxLength 
+  for(let i = 0; i <maxLength; i++){
+    divider += "-"
+  }
+
+  // find the row with the most spaces
+  
+
+  for (let i = 0; i < diff.length; i++) {
+    currRowSpace = 0
+    for (let j = 0; j < diff[i].length-1; j++) {
+      currRowSpace += diff[i][j]
+    }
+    rowTotalSpace.push(currRowSpace)
+  }
+  let RS = Math.max(...rowTotalSpace)
+  // account for the total spaces
+  console.log("totoal spaces are:   " + rowTotalSpace)
+  for (let i = 0; i < RS; i++) {
+    divider += "-"
+  }
+  return divider;
+    
+}
+
+function makeTable(Arr, diff, divider){
+  var spaces = ""
+  var countPerItem = 0
+
   console.log(diff)
   for (let i = 0; i < Arr.length; i++) {
     for (let j = 0; j < Arr[i].length; j++) {
-      count = diff[i][j]
+      countPerItem = diff[i][j]
       spaces = ""
-      for (let m = 0; m < count; m++) {
+      for (let m = 0; m < countPerItem; m++) {
         spaces += " "
       }
-      process.stdout.write("|" + Arr[i][j] + spaces)
+      process.stdout.write("|" + Arr[i][j] + spaces + " ")
     }
+    process.stdout.write("|")
       console.log('')
       console.log(divider)
   }
 }
-
 
 
 
--- jasmineGUIDE.md
+++
@@ -1,2 +0,0 @@
-npx jasmine init
-in scripts "tests": "jasmine"
